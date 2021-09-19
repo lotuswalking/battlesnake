c85668e4c1b5fffe1d200e9b79bfc64309203383
lotuswalking <jiangzhonping@163.com>
2021-09-19 14:32:06 -0400

    update version

--- aisnake.js
+++ aisnake.js
@@ -5,6 +5,16 @@ function moreUpdate(){
         direction = "r"
     }
     if (!gameOver){
+<<<<<<< Updated upstream
         direction = ais['cicheng'](gridSize,snake,apples,direction);
+=======
+        if (ai == ""){
+            ai = 'junyan';
+        }
+        d = ais[ai](gridSize,snake,apples,direction);
+        if (["r","l","u","d"].includes(d)){
+            direction = d;
+        }
+>>>>>>> Stashed changes
     }
 }
